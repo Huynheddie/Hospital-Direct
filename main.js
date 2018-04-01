1f8f69a1264bd2e4fb4d56a39b845ad7c8da86d1
Eddie Huynh <huynheddie@yahoo.com>
2018-03-31 22:20:58 -0700

    Added JS functions and linked pages to the file

--- main.js
+++ main.js
@@ -11,7 +11,18 @@ class Patient {
 		this.provider = provider;
 		this.insurance_num = insurance_num;
 		this.records[];
-		this.hospitals[];
+		this.hospital;
+	}
+	constructor(p1) {
+		this.id = id_p++;
+		this.name = p1.name;
+		this.dob = p1.dob;
+		this.number = p1.number;
+		this.address = p1.address;
+		this.provider = p1.provider;
+		this.insurance_num = p1.insurance_num;
+		this.records[] = p1.records;
+		this.hospital;
 	}
 }
 
@@ -25,10 +36,14 @@ class Hospital {
 		this.password = password;
 		this.patients[];
 	}
+	receivePatient(p1) {
+		Patient np = new Patient(p1);
+		this.patients.push(np);
+	}
 }
 
 class Record {
-	constructor(id, height, weight, bp, temp, notes, date) {
+	constructor(id, height, weight, bp, temp, notes) {
 		this.id = id;
 		this.height = height;
 		this.weight = weight;
@@ -55,6 +70,37 @@ class System {
 		this.allHospitals.push(First);
 		Hospital Second = new Hospital("UCI Medical Center", "714-456-7890", "Pavilion 4, 101 The City Dr S, Orange, CA 92868", "admin@uci.edu", "uci")		
 		this.allHospitals.push(Second);
+	}
+	createPatient(name, dob, number, address, provider, insurance_num, h_username){
+		var h_index = findRecord(h_username);
+		Patient p1 = new Patient(name, dob, number, address, provider, insurance_num);
+		allHospitals[h_index].patients.push(p1);
+	}
+	createRecord(id, height, weight, bp, temp, notes, h_username, p_index) {
+		var h_index = findRecord(h_username);
+		Record r1 = new Record(id, height, weight, bp, temp, notes);
+		allHospitals[h_index].patients[p_index].records.push(r1);
+	}
+	sendRecord(hospital_username, patient){
+		var h_index = findRecord(h_username);
+		allHospitals[h_index].receivePatient(patient);
+	}
+	findRecord(username) {
+		var total_records = this.allHospitals.length;
+		for (let i = 0; i < total_records; i++) {
+			if (this.allHospitals[i].username === username) {
+				return i;
+			}
+		}
+	}
+	findPatient(patientId, h_username){
+		var h_index = findRecord(h_username);
+		var total_records = this.allHospitals[h_index].patients.length;
+		for (let i = 0; i < total_records; i++) {
+			if (this.allHospitals[h_index].patients[i].id === patientId) {
+				return i;
+			}
+		}
 	}
 
 }
